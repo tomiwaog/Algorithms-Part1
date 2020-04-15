21e9c7aa3b5539099fa2664970fbecc11e58580e
Tomiwa Ogunbamowo <contact@tomiwa.io>
2020-04-15 04:23:02 +0100

    Test implementation of Elementary Sorting Algorithms

--- src/test/TestSort.java
+++ src/test/TestSort.java
@@ -10,61 +10,61 @@ import sorting.Sort;
 public class TestSort {
 
     @Test
-    public void testIsSortedAsc(){
-        int [] arr = new int []{-3,0,4,18,18,20};
+    public void testIsSortedAsc() {
+        int[] arr = new int[] { -3, 0, 4, 18, 18, 20 };
         boolean sorted = Sort.isSortedAsc(arr);
         Assert.assertEquals(true, sorted);
     }
-    
+
     @Test
-    public void testIsSortedEq(){
-        int [] arr = new int []{20,20,20,20};
+    public void testIsSortedEq() {
+        int[] arr = new int[] { 20, 20, 20, 20 };
         boolean sorted = Sort.isSortedAsc(arr);
         Assert.assertEquals(true, sorted);
     }
-    
+
     @Test
-    public void testIsSortedAscFalse(){
-        int [] arr = new int []{-3,0,4,3,18,18,20};
+    public void testIsSortedAscFalse() {
+        int[] arr = new int[] { -3, 0, 4, 3, 18, 18, 20 };
         boolean sorted = Sort.isSortedAsc(arr);
         Assert.assertEquals(false, sorted);
     }
-    
+
     @Test
-    public void testIsSortedDesc(){
-        int [] arr = new int []{10,5,0,-5,-5,-9};
+    public void testIsSortedDesc() {
+        int[] arr = new int[] { 10, 5, 0, -5, -5, -9 };
         boolean sorted = Sort.isSortedAsc(arr);
         Assert.assertEquals(false, sorted);
     }
-    
+
     @Test
-    public void testIsSortedDescFalse(){
-        int [] arr = new int []{10,5,0,-5, -4,-5,-9};
+    public void testIsSortedDescFalse() {
+        int[] arr = new int[] { 10, 5, 0, -5, -4, -5, -9 };
         boolean sorted = Sort.isSortedAsc(arr);
         Assert.assertEquals(false, sorted);
     }
-    
+
     @Test
-    public void testInsertionSort(){
-        int [] arr = new int []{10,5,0,-5,-5,-9,21,-1};
+    public void testInsertionSort() {
+        int[] arr = new int[] { 10, 5, 0, -5, -5, -9, 21, -1 };
         Sort.insertionSort(arr);
         boolean sorted = Sort.isSortedAsc(arr);
         System.out.println(Arrays.toString(arr));
         Assert.assertEquals(true, sorted);
     }
-    
+
     @Test
-    public void testSelectionSort(){
-        int [] arr = new int []{10,5,0,-5,-5,-9,21,-1};
+    public void testSelectionSort() {
+        int[] arr = new int[] { 10, 5, 0, -5, -5, -9, 21, -1 };
         Sort.selectionSort(arr);
         boolean sorted = Sort.isSortedAsc(arr);
         System.out.println(Arrays.toString(arr));
         Assert.assertEquals(true, sorted);
     }
-    
+
     @Test
-    public void testBubbleSort(){
-        int [] arr = new int []{13, 7, 6, 45, 21, 9, 101, 102, -1};
+    public void testBubbleSort() {
+        int[] arr = new int[] { 13, 7, 6, 45, 21, 9, 101, 102, -1 };
         Sort.bubbleSort(arr);
         boolean sorted = Sort.isSortedAsc(arr);
         System.out.println(Arrays.toString(arr));
