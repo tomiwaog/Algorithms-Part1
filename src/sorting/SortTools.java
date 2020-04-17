8fcacd9c7f39d9b187b9e994a37e6040a2d1b4cb
Tomiwa Ogunbamowo <contact@tomiwa.io>
2020-04-17 13:20:16 +0100

    Refactored All Sort Algorithms to accept Generic Types.

--- src/sorting/SortTools.java
+++ src/sorting/SortTools.java
@@ -1,7 +1,7 @@
 package sorting;
 
-public class Sort {
-    public static void insertionSort(int[] arr){
+public class SortTools{
+    public static void insertionSort(Comparable[] arr){
         for (int i=0; i<arr.length;i++){
             for (int j=i; j>0; j--){
                 if (isLess(arr, j, j-1)) swap(arr, j, j-1);
@@ -9,18 +9,18 @@ public class Sort {
             }
         }
     }
-    
-    public static void selectionSort(int [] arr){
+
+    public static void selectionSort(Comparable [] arr){
         for (int i=0; i<arr.length;i++){
             int min=i;
             for (int j=i+1; j<arr.length;j++){
-                if (Sort.isLess(arr, j, min)) min=j;
+                if (isLess(arr, j, min)) min=j;
             }
             swap(arr, i,min);
         }
     }
     
-    public static void shellSort(int[] arr) {
+    public static void shellSort(Comparable[] arr) {
         int length = arr.length;
         int gap = 1;
         while (gap < length / 3)
@@ -28,36 +28,37 @@ public class Sort {
         
         while (gap >= 1) {
             for (int i = gap; i < length; i++) {
-                for (int j = i; j >= gap && Sort.isLess(arr, j, j - gap); j -= gap) {
-                    Sort.swap(arr, j, j - gap);
+                for (int j = i; j >= gap && isLess(arr, j, j - gap); j -= gap) {
+                    swap(arr, j, j - gap);
                 }
             }
             gap = gap / 3;
         }
     }
-    public static void bubbleSort(int [] arr){
+    public static void bubbleSort(Comparable [] arr){
         for (int i=arr.length-1; i>=0; i--){
             for (int j=0;j<i;j++){
-                if (Sort.isLess(arr, j+1, j)) Sort.swap(arr, j, j+1);
+                if (isLess(arr, j+1, j)) swap(arr, j, j+1);
             }
         }
     }
     
-    public static boolean isSortedAsc(int[] arr){
+    public static boolean isSortedAsc(Comparable[] arr){
         for (int i=0; i<arr.length-1;i++){
-            if (arr[i]>arr[i+1])return false;
+            if (isLess(arr, i+1,i))return false;
         }
         return true;   
     }
     
-    public  static void swap(int[] arr, int i, int j){
-        int temp = arr[i];
+    public  static void swap(Comparable[] arr, int i, int j){
+        Comparable temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
     
-    
-    public static boolean isLess(int [] arr, int i, int j){
-       return (arr[i]< arr[j]);
+    public static boolean isLess(Comparable [] arr, int i, int j){
+        return arr[i].compareTo(arr[j])<0;
     }
+
+
 }
---
+++ src/test/TestSortTools.java
@@ -0,0 +1,103 @@
+package test;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import sorting.SortTools;
+
+public class TestSortTools {
+
+    @Test
+    public void testisSortedAsc() {
+        Integer[] arr = new Integer[] { -3, 0, 4, 18, 18, 20 };
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(true, SortToolsed);
+    }
+    @Test
+    public void testisSortedAscStrFalse() {
+        String[] arr = new String[] {"bbb","bbc", "bab", "aaa"};
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(false, SortToolsed);
+    }
+    
+    @Test
+    public void testisSortedAscStr() {
+        String[] arr = new String[] {"aaa", "bab", "bbb","bbc",};
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(true, SortToolsed);
+    }
+
+    @Test
+    public void testIsSortedEq() {
+        Integer[] arr = new Integer[] { 20, 20, 20, 20 };
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(true, SortToolsed);
+    }
+
+    @Test
+    public void testisSortedAscFalse() {
+        Integer[] arr = new Integer[] { -3, 0, 4, 3, 18, 18, 20 };
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(false, SortToolsed);
+    }
+    
+
+    @Test
+    public void testIsSortedDesc() {
+        Integer[] arr = new Integer[] { 10, 5, 0, -5, -5, -9 };
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(false, SortToolsed);
+    }
+
+    @Test
+    public void testIsSortedDescFalse() {
+        Integer[] arr = new Integer[] { 10, 5, 0, -5, -4, -5, -9 };
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        Assert.assertEquals(false, SortToolsed);
+    }
+
+    @Test
+    public void testInsertionSort() {
+        Integer[] arr = new Integer[] { 10, 5, 0, -5, -5, -9, 21, -1 };
+        SortTools.insertionSort(arr);
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        System.out.println(Arrays.toString(arr));
+        Assert.assertEquals(true, SortToolsed);
+    }
+
+    @Test
+    public void testSelectionSort() {
+        Integer[] arr = new Integer[] { 10, 5, 0, -5, -5, -9, 21, -1 };
+        SortTools.selectionSort(arr);
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        System.out.println(Arrays.toString(arr));
+        Assert.assertEquals(true, SortToolsed);
+    }
+
+    @Test
+    public void testBubbleSort() {
+        Integer[] arr = new Integer[]{ 13, 7, 6, 45, 21, 9, 101, 102};
+        SortTools.bubbleSort(arr);
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        System.out.println(Arrays.toString(arr));
+        Assert.assertEquals(true, SortToolsed);
+    }
+    
+    @Test public void testShellSort(){
+        Integer[] arr = new Integer[] { 100, 80, 70, 60, 90, 40, 30, 20, 10, -3,14 };
+        SortTools.shellSort(arr);
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        System.out.println(Arrays.toString(arr));
+        Assert.assertEquals(true, SortToolsed);
+    }
+    
+    @Test
+    public void testShellSortString() {
+        String[] arr = new String[] {"bbb","bbc", "bab", "aaa"};
+        SortTools.shellSort(arr);
+        boolean SortToolsed = SortTools.isSortedAsc(arr);
+        System.out.println(Arrays.toString(arr));
+        Assert.assertEquals(true, SortToolsed);
+    }
+}
